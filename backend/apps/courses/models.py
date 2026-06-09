cfd52313aeb7a0db92e6846ee7b4e48d78c7b1f3
GGerryHub <salinasdelacruz.gerardo@utacapulco.edu.mx>
2026-06-08 19:18:48 -0600

    Add archivado field to Course

--- backend/apps/courses/models.py
+++ backend/apps/courses/models.py
@@ -26,6 +26,7 @@ class Course(models.Model):
     fecha_fin = models.DateField(null=True, blank=True)
     # Only one course per admin can be activo=True at a time
     activo = models.BooleanField(default=False)
+    archivado = models.BooleanField(default=False)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
 
